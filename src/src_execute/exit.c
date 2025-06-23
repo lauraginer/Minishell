06c1e06e3a850e8c90f02e77d66d5f92d4c6b63f
lauraginer <lauginmat@gmail.com>
2025-06-23 22:02:35 +0200

    subido, investiga bien los pipe los procesos y lo que demonios sea

--- src/src_execute/exit.c
+++ src/src_execute/exit.c
@@ -6,31 +6,49 @@
 /*   By: lginer-m <lginer-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/19 22:35:35 by lginer-m          #+#    #+#             */
-/*   Updated: 2025/06/20 21:49:11 by lginer-m         ###   ########.fr       */
+/*   Updated: 2025/06/23 21:36:23 by lginer-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
+int control_nums(char *str)
+{
+	int i;
+
+	i = 0;
+	if(str[i] == '-' || str[i] == '+')
+		i++;
+	while(str[i])
+	{
+		if(!ft_isdigit(str[i]));
+			return(0);
+		i++;	
+	}
+	return(1);
+}
 int builtin_exit(char **args)
 {
 	if(args[1])
 	{
 		if (args[2])
 		{
-			ft_printf("exit\n");
-			ft_printf("exit: too many arguments\n");
+			printf("exit\n");
+			printf("exit: too many arguments\n");
 		}
-		if (!args[1] && !(ft_isalnum(ft_atoi(args[1])))) //en este caso sale con el código 255 por convención
+		if (!args[1] && !(control_nums(args[1]))) //en este caso sale con el código 255 por convención
 		{
-			ft_printf("exit\n");
-			ft_printf("exit: '%s': numeric argument required\n", args[1]);
+			printf("exit\n");
+			printf("exit: '%s': numeric argument required\n", args[1]);
 			//sale por convención
-			return(0);
+			return(255);
 		}	
 		else
 			//actua el comando tal y como siempre, siempre y cuando sea un numero digito
 		return(0);
 	}
+
+	/*importante la variable exit(valor de retorno, hay que actualizarlo conforme cada opcion Y SIEMPRE en comandos/builtins),
+	 el exit solo se hace en un proceso padre*/
 	
 }
--- src/src_execute/test_env.c
+++ src/src_execute/test_env.c
@@ -6,7 +6,7 @@
 /*   By: lginer-m <lginer-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/19 22:05:00 by lginer-m          #+#    #+#             */
-/*   Updated: 2025/06/19 22:57:20 by lginer-m         ###   ########.fr       */
+/*   Updated: 2025/06/23 19:43:01 by lginer-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ t_list *copy_env_var(char **envp); // Ajusta la firma según tu implementación 
 
 int main(int argc, char **argv, char **envp)
 {
-	/*t_list *my_env;
+	t_list *my_env;
 	
 	(void)argc;
 	
 	// Crear una copia de las variables de entorno
-	my_env = copy_env_var(envp);
+	/*my_env = copy_env_var(envp);
 	if (!my_env)
 	{
 		ft_printf("Error: No se pudo copiar el entorno\n");
@@ -33,8 +33,9 @@ int main(int argc, char **argv, char **envp)
 	int result = builtin_env(argv, my_env);
 	
 	// Liberar memoria (ajusta según tu implementación)
-	// free_env_list(my_env);*/
+	// free_env_list(my_env);
 
-	builtin_echo(argv);
+	//builtin_echo(argv);*/
+	builtin_pwd(argv);
 	return (0);
 }
