9bf75c64398232c0f9814df382d75ff797b34c4c
lauraginer <lauginmat@gmail.com>
2025-07-15 20:30:28 +0200

    actualizado xd

--- src/src_execute/unset.c
+++ src/src_execute/unset.c
@@ -6,12 +6,13 @@
 /*   By: lginer-m <lginer-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 21:03:33 by lginer-m          #+#    #+#             */
-/*   Updated: 2025/07/15 18:03:38 by lginer-m         ###   ########.fr       */
+/*   Updated: 2025/07/15 20:30:07 by lginer-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
+int remove_env(char *var, t_list **my_env);
 builtin_unset(char **args, t_list **my_env, t_ms *ms)
 {
 	t_list *current_env;
@@ -20,3 +21,4 @@ builtin_unset(char **args, t_list **my_env, t_ms *ms)
 	ms->exit_status = 0;
 	
 }
+//NO GESTIONA SI LA NOMENCLATURA ES CORRECTA
