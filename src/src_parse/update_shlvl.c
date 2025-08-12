c240fafaacc005d2dc4ba3ad03e31ee4bf1162eb
Jose Ismael Caro Lopez <jcaro-lo@c3r6s6.42malaga.com>
2025-08-12 16:57:14 +0200

    fucking header

--- src/src_parse/update_shlvl.c
+++ src/src_parse/update_shlvl.c
@@ -14,19 +14,21 @@ void	increase_shlvl(t_ms *ms)
 	{
 		if (ft_strncmp(((char *)ms->my_env->content), "SHLVL=", 6) == 0)
 		{
-			shlvl = ft_substr(((char *)ms->my_env->content), 6, (ft_strlen(((char *)ms->my_env->content) - 6)));
+			shlvl = ft_substr(((char *)ms->my_env->content), 6,
+					(ft_strlen(((char *)ms->my_env->content) - 6)));
 			lvl = ft_atoi(shlvl);
 			lvl++;
 			free(shlvl);
 			shlvl = ft_itoa(lvl);
 			free(ms->my_env->content);
-			ms->my_env->content = ft_strjoin("SHLVL=" , shlvl);
+			ms->my_env->content = ft_strjoin("SHLVL=", shlvl);
 			free(shlvl);
 			break ;
 		}
 		aux = aux->next;
 	}
 }
+
 void	decrease_shlvl(t_ms *ms)
 {
 	char	*shlvl;
@@ -40,13 +42,14 @@ void	decrease_shlvl(t_ms *ms)
 	{
 		if (ft_strncmp(((char *)ms->my_env->content), "SHLVL=", 6) == 0)
 		{
-			shlvl = ft_substr(((char *)ms->my_env->content), 6, (ft_strlen(((char *)ms->my_env->content) - 6)));
+			shlvl = ft_substr(((char *)ms->my_env->content), 6,
+					(ft_strlen(((char *)ms->my_env->content) - 6)));
 			lvl = ft_atoi(shlvl);
 			lvl--;
 			free(shlvl);
 			shlvl = ft_itoa(lvl);
 			free(ms->my_env->content);
-			ms->my_env->content = ft_strjoin("SHLVL=" , shlvl);
+			ms->my_env->content = ft_strjoin("SHLVL=", shlvl);
 			free(shlvl);
 			break ;
 		}
